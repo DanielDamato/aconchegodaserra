775e63e6a81b80174ba7ff14c6945dd35912b5e2
Daniel Santoro D'Amato <daniel.damato88@gmail.com>
2017-04-27 15:26:44 -0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -36,8 +36,7 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 	} else {
 		$error = 'Mensagem enviada!';
 		return true;
-	}
-}
+	}}
 
 // Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
 //o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
